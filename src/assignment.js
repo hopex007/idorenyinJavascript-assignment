39cd78fad0066db6b1609beecdcddca773573d6c
bassey idorenyin inyang <hopexbassey100@gmail.com>
2022-07-15 00:16:29 +0000

    i just completed the javascript challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    sumOfNumbers=0;
+  for(let i=0; i<arrayOfNumbers.length; i++){
+    sumOfNumbers+=arrayOfNumbers[i];
+  }
+    return sumOfNumbers;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+     const evenNumber = [];
+
+  arrayOfNumbers.forEach(number => {
+    if (number % 2 === 0) {
+      evenNumber.push(number)
+    }
+  })
+    return evenNumber.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
